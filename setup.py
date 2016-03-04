87af00617a67fb7925a34599d2d34b6840d670f1
kj <redacted>
2016-03-04 11:46:33 -0500

    incremented version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'quickconfig',
     py_modules=['quickconfig'],
-    version = '2.0',
+    version = '2.1',
     description = 'Python configuration tool.',
     author = 'KJ',
     author_email = '<see github for support>',
     url = 'https://github.com/jdotpy/quickconfig',
-    download_url = 'https://github.com/jdotpy/quickconfig/tarball/2.0',
+    download_url = 'https://github.com/jdotpy/quickconfig/tarball/2.1',
     keywords = ['configuration', 'config', 'settings', 'tools'],
     classifiers = [],
 )
